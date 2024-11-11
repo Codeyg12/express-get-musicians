91861932f1bffdb6466a2309ce4e67b4bcbe9902
Codey Gallup <codey.gallup@gmail.com>
2024-11-11 12:18:07 -0700

    Adding first route and proceeding tests

--- index.test.js
+++ index.test.js
@@ -12,9 +12,23 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test("get should work and respond with 200", async () => {
+        const res = await request(app).get("/musicians")
+        expect(res.statusCode).toBe(200)
+    })
     
+    test("should respond with the seeded data", async () => {
+        const res = await request(app).get("/musicians")
+        const resData = JSON.parse(res.text)
+        expect(resData).toHaveLength(3)
+    })
     
-
+// Day 1 Extension
+    //     Extension Problems
+    // Create routes for GET /musicians/1 that returns the first item in the database. Repeat for the other items in the database.
+    // A Band model has been created for you. Create a GET /bands route that returns all Band data in the database by doing the following:
+    // Create the GET /bands route.
+    // Create tests to verify that your /bands endpoint works as expected!
 
 
 
